110f513716573583ee77cdcc79718c6ba1f9706e
DarkRiDDeR <darkridder.pro@gmail.com>
2018-05-16 04:01:01 +0800

    add tests

--- source/app.d
+++ source/app.d
@@ -11,11 +11,20 @@ import jsonizer.jsonize;
 import jsonizer.fromjson;
 import jsonizer.tojson;
 
+import drmi.sbin;
+
+pragma(lib, "ws2_32");
 import msgpack;
 
-/*import orange.serialization._;
-import orange.serialization.archives._;
-*/
+
+void printResults (long size, int itersCount, Duration serializeResult, Duration deSerializeResult)
+{
+    write("serialize test = ");
+    write(serializeResult);
+    writeln (format!(". Size = %s")(cast(long)size * cast(long)itersCount));
+    write("deserialize test = ");
+    writeln(deSerializeResult);
+}
 
 void testCerealed(T)(T data, int itersCount)
 {
@@ -30,51 +39,8 @@ void testCerealed(T)(T data, int itersCount)
         serializeData.decerealize!T;
     }
     auto r = benchmark!(serializeTest, deSerializeTest)(itersCount);
-
-    Duration serializeResult = r[0];
-    Duration deSerializeResult = r[1];
-
-    write("serialize test = ");
-    write(serializeResult);
-    writeln (format!(". Size = %s")(ubyte.sizeof * serializeData.length * itersCount));
-    write("deserialize test = ");
-    writeln(deSerializeResult);
+    printResults(cast(long)(serializeData.sizeof * serializeData.length), itersCount, r[0], r[1]);
 }
-
-
-/*void testOrange(T)(T data, int itersCount)
-{
-    auto archive = new XmlArchive!(char); // create an XML archive
-	auto serializer = new Serializer(archive); // create the serializer
-
-	serializer.serialize(data); // serialize
-
-
-    auto deSerializeData = serializer.deserialize!(T)(archive.untypedData);
-    assert(deSerializeData == data);
-
-
-    ubyte[] serializeData = data.cerealise;
-    auto deSerializeData = serializeData.decerealize!T;
-    assert(deSerializeData == data);
-
-    void serializeTest() {
-        data.cerealise;
-    }
-    void deSerializeTest() {
-        serializeData.decerealize!T;
-    }
-    auto r = benchmark!(serializeTest, deSerializeTest)(itersCount);
-
-    Duration serializeResult = r[0];
-    Duration deSerializeResult = r[1];
-
-    write("serialize test = ");
-    write(serializeResult);
-    writeln (format!(". Size = %s")(ubyte.sizeof * serializeData.length * itersCount));
-    //write("deserialize test = ");
-    //writeln(deSerializeResult);
-}*/
 
 void testJsonizer(T)(T data, int itersCount)
 {
@@ -90,42 +56,106 @@ void testJsonizer(T)(T data, int itersCount)
         serializeData.fromJSONString!T;
     }
     auto r = benchmark!(serializeTest, deSerializeTest)(itersCount);
-
-    Duration serializeResult = r[0];
-    Duration deSerializeResult = r[1];
-
-    write("serialize test = ");
-    write(serializeResult);
-    writeln (format!(". Size = %s")(serializeData.sizeof * serializeData.length * itersCount));
-    write("deserialize test = ");
-    writeln(deSerializeResult);
+    printResults(cast(long)(serializeData.sizeof * serializeData.length), itersCount, r[0], r[1]);
 }
 
-
-void testJson(T)(T data, int itersCount)
+void testMsgPack(T)(T data, int itersCount)
 {
-    auto serializeData = data.toJSONString;
-    auto deSerializeData = serializeData.fromJSONString!T;
+    ubyte[] serializeData = pack(data);
+    auto deSerializeData = msgpack.unpack(serializeData).as!(T);
 
     assert(deSerializeData == data);
 
     void serializeTest() {
-        data.toJSONString;
+        pack(data);
     }
     void deSerializeTest() {
-        serializeData.fromJSONString!T;
+        msgpack.unpack(serializeData).as!(T);
     }
     auto r = benchmark!(serializeTest, deSerializeTest)(itersCount);
-
-    Duration serializeResult = r[0];
-    Duration deSerializeResult = r[1];
-
-    write("serialize test = ");
-    write(serializeResult);
-    writeln (format!(". Size = %s")(serializeData.sizeof * serializeData.length * itersCount));
-    write("deserialize test = ");
-    writeln(deSerializeResult);
+    printResults(cast(long)(serializeData.sizeof * serializeData.length), itersCount, r[0], r[1]);
 }
+
+void testDrmi(T)(T data, int itersCount)
+{
+    auto serializeData = data.sbinSerialize;
+    auto deSerializeData = serializeData.sbinDeserialize!T;
+
+    assert(deSerializeData == data);
+
+    void serializeTest() {
+        data.sbinSerialize;
+    }
+    void deSerializeTest() {
+        serializeData.sbinDeserialize!T;
+    }
+    auto r = benchmark!(serializeTest, deSerializeTest)(itersCount);
+    printResults(cast(long)(serializeData.sizeof * serializeData.length), itersCount, r[0], r[1]);
+}
+
+
+void testJsonBook(StructBook data, int itersCount)
+{
+    JSONValue jj = ["name": data.name];
+    jj.object["pageNumber"] = JSONValue(data.pageNumber);
+    jj.object["published"] = JSONValue(data.published);
+    jj.object["author"] = JSONValue(data.author);
+    jj.object["description"] = JSONValue(data.description);
+
+    auto serializeData = jj.toString;
+    auto deSerializeData = parseJSON(serializeData);
+
+    assert(deSerializeData["name"].str == data.name);
+    assert(deSerializeData["pageNumber"].integer == data.pageNumber);
+    assert(deSerializeData["published"].str == data.published);
+    assert(deSerializeData["author"].str == data.author);
+    assert(deSerializeData["description"].str == data.description);
+
+    void serializeTest() {
+        JSONValue jj = ["name": data.name];
+        jj.object["pageNumber"] = JSONValue(data.pageNumber);
+        jj.object["published"] = JSONValue(data.published);
+        jj.object["author"] = JSONValue(data.author);
+        jj.object["description"] = JSONValue(data.description);
+        jj.toString;
+    }
+    void deSerializeTest() {
+        deSerializeData = parseJSON(serializeData);
+        deSerializeData["name"].str;
+        deSerializeData["pageNumber"].integer;
+        deSerializeData["published"].str;
+        deSerializeData["author"].str;
+        deSerializeData["description"].str;
+    }
+    auto r = benchmark!(serializeTest, deSerializeTest)(itersCount);
+    printResults(serializeData.sizeof * serializeData.length, itersCount, r[0], r[1]);
+}
+
+void testJsonPage(StructPage data, int itersCount)
+{
+    JSONValue jj = ["data": data.data];
+    jj.object["time"] = JSONValue(data.time);
+
+    auto serializeData = jj.toString;
+    auto deSerializeData = parseJSON(serializeData);
+
+    assert(deSerializeData["data"].str == data.data);
+    assert(deSerializeData["time"].integer == data.time);
+
+    void serializeTest() {
+        JSONValue jj = ["data": data.data];
+        jj.object["time"] = JSONValue(data.time);
+        jj.toString;
+    }
+    void deSerializeTest() {
+        deSerializeData = parseJSON(serializeData);
+        deSerializeData["data"].str;
+        deSerializeData["time"].integer;
+    }
+    auto r = benchmark!(serializeTest, deSerializeTest)(itersCount);
+    printResults(serializeData.sizeof * serializeData.length, itersCount, r[0], r[1]);
+}
+
 
 struct StructBook {
     string name = "Alice's Adventures in Wonderland";
@@ -135,7 +165,7 @@ struct StructBook {
     string description = "Alice's Adventures in Wonderland (commonly shortened to Alice in Wonderland) is an 1865 novel written by English author Charles Lutwidge Dodgson under the pseudonym Lewis Carroll.";
 }
 struct StructPage {
-    string data = import("w3c.html");
+    string data = import("w3c.html"); // 39 kb
     int time = 1522852522; // Epoch timestamp
 }
 struct StructLong {
@@ -194,29 +224,41 @@ void main()
     StructPageJsonizer pageJsonizer;
     StructLongJsonizer longDJsonizer;
 
-
     writeln("===CEREALED===");
     writeln("book");
-    testCerealed!StructBook(book, 100_000);
+    testCerealed!StructBook(book, 10_000);
     writeln("page cache");
-    testCerealed!StructPage(page, 100_000);
+    testCerealed!StructPage(page, 10_000);
     writeln("long structure");
-    testCerealed!StructLong(longD, 100_000);
-
-
-    /*writeln("===ORANGE===");
-    writeln("book");
-    testOrange!StructBook(book, 10_000);
-    writeln("page cache");
-    testOrange!StructPage(page, 10_000);
-    writeln("long structure");
-    testOrange!StructLong(longD, 10_000);*/
+    testCerealed!StructLong(longD, 10_000);
 
     writeln("===JSONIZER===");
     writeln("book");
-    testJsonizer!StructBookJsonizer(bookJsonizer, 100_000);
+    testJsonizer!StructBookJsonizer(bookJsonizer, 10_000);
     writeln("page cache");
-    testJsonizer!StructPageJsonizer(pageJsonizer, 100_000);
+    testJsonizer!StructPageJsonizer(pageJsonizer, 10_000);
     writeln("long structure");
-    testJsonizer!StructLongJsonizer(longDJsonizer, 100_000);
+    testJsonizer!StructLongJsonizer(longDJsonizer, 10_000);
+
+    writeln("===MessagePack===");
+    writeln("book");
+    testMsgPack!StructBook(book, 10_000);
+    writeln("page cache");
+    testMsgPack!StructPage(page, 10_000);
+    writeln("long structure");
+    testMsgPack!StructLong(longD, 10_000);
+
+    writeln("===DRMI===");
+    writeln("book");
+    testDrmi!StructBook(book, 10_000);
+    writeln("page cache");
+    testDrmi!StructPage(page, 10_000);
+    writeln("long structure");
+    testDrmi!StructLong(longD, 10_000);
+
+    writeln("===Json===");
+    writeln("book");
+    testJsonBook(book, 10_000);
+    writeln("page cache");
+    testJsonPage(page, 10_000);
 }
